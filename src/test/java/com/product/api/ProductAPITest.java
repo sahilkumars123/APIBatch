8b5c1bcbee94e6bc1b779d26c3f234c78f7a0107
Sahil Kumar <sahil.kumar9274@gmail.com>
2024-02-13 19:22:49 +0530

    Lombok API

--- src/test/java/com/product/api/ProductAPITest.java
+++ src/test/java/com/product/api/ProductAPITest.java
@@ -55,4 +55,47 @@ public class ProductAPITest {
 		}
 	}
 
+	
+	
+	@Test
+	public void productTest_WithLombokAPITest() {
+		
+	    RestAssured.baseURI = "https://fakestoreapi.com";
+			
+			Response response =  given()
+				.when()
+					.get("/products");
+			
+			//json
+			
+			//json to pojo
+			
+			ObjectMapper mapper = new ObjectMapper();
+			
+			try {
+				
+			 ProductLombok[] product = mapper.readValue(response.getBody().asString(), ProductLombok[].class);
+			 
+			 
+			 for(ProductLombok p : product) {
+					System.out.println("ID:: "+p.getId());
+					System.out.println("Title:: "+p.getTitle());
+					System.out.println("Price:: "+p.getPrice());
+					System.out.println("Description:: "+p.getDescription());
+					System.out.println("Category:: "+p.getCategory());
+					System.out.println("Image:: "+p.getImage());
+					System.out.println("Rating Rate:: "+p.getRating().getRate());
+					System.out.println("Count Rate:: "+p.getRating().getCount());
+
+					System.out.println("------------------------------------");
+				}
+			
+			} catch (JsonMappingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 }
---
+++ src/test/java/com/product/api/ProductLombok.java
@@ -0,0 +1,32 @@
+package com.product.api;
+
+import com.product.api.Product.Rating;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductLombok {
+	
+	private int id;
+	private String title;
+	private double price;
+	private String description;
+	private String category;
+	private String image;
+	private Rating rating;
+	
+	
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	public static class Rating{
+		private double rate;
+		private int count;
+		
+	}
+}
